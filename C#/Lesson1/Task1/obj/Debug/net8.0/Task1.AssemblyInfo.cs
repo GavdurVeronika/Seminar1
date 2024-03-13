c02278c7021127e5016be0da162cb2e0ba314f2c
Veronika <gavdur.veronika@gmail.ru>
2024-03-13 16:46:22 +0300

    add change

--- C#/Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ C#/Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+707b5e721b8442c06eff4c4057f3f75e33ade27d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ C#/Lesson1/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 57;
+int b = 75;
+if(a > b)
+{
+   Console.WriteLine(a);
+ }
+ else
+ {
+    Console.WriteLine(b);
+ }
